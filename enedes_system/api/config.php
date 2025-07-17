933f7ff2657b9454e8612983fb4fd81dff04724a
EnedesIF <2148722@etfbsb.edu.br>
2025-07-17 14:44:39 -0300

    Update config.php

--- enedes_system/api/config.php
+++ enedes_system/api/config.php
@@ -1,10 +1,9 @@
 <?php
 $host = 'ep-sweet-bar-aec2ww8z-pooler.c-2.us-east-2.aws.neon.tech';
 $db   = 'neondb';
-$user = 'neondb_owner;
+$user = 'neondb_owner';  // ← ASPA CORRIGIDA
 $pass = 'npg_igfwntzh0j1C';
 $port = "5432";
-
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db;sslmode=require", $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
