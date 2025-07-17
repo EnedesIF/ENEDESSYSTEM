9abbf113adccd4c5d6dbdc740b9f341ca371e6bd
EnedesIF <2148722@etfbsb.edu.br>
2025-07-17 11:41:32 -0300

    Delete enedes_system/api directory

--- enedes_system/api/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Exemplo de função usando conexão global PDO
-function buscarUsuarios() {
-    global $pdo;
-    $stmt = $pdo->query("SELECT * FROM usuarios");
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-?>
